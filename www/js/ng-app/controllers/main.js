143bcbb7986dc82e8fa85c60cd48d61fcd9fcdd4
Jazz <jeffersongalano@Jeffersons-MacBook-Pro.local>
2015-05-16 02:56:47 +0800

    Added the controllers and the datacontext for getting the places

--- www/js/ng-app/controllers/main.js
+++ www/js/ng-app/controllers/main.js
@@ -1,2 +1,10 @@
-angular
+var appController = angular
   .module("appController", ['appService']);
+
+
+appController.controller("indexController", ['$scope', 'dataContext', function($scope, dataContext){
+  dataContext.getPlaces().then(function(data){
+    $scope.data = data.data;
+  });
+  
+}])
--- www/js/ng-app/services/main.js
+++ www/js/ng-app/services/main.js
@@ -1,2 +1,18 @@
-angular
-  .module("appService", []);
+(function() {
+  'use strict';
+
+  var appservice = angular.module("appService", []);
+
+  appservice.factory("dataContext", ['$http', function ($http) {
+    alert("test");
+    var service = {
+      getPlaces: getPlaces
+    }
+
+    return service;
+
+    function getPlaces(){
+      return $http.get('/api/places.json');
+    }
+  }]);
+})();
